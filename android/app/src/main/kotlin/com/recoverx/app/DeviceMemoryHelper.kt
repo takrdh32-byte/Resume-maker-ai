512ab22b73844465d575eb7dfb0f36340b3c4f0d
takrdh32-byte <takrdh32@gmail.com>
2026-07-01 00:48:03 +0530

    Update DeviceMemoryHelper.kt

--- android/app/src/main/kotlin/com/recoverx/app/DeviceMemoryHelper.kt
+++ android/app/src/main/kotlin/com/recoverx/app/DeviceMemoryHelper.kt
@@ -1,16 +1,12 @@
 package com.recoverx.app
-
 import android.app.ActivityManager
 import android.content.Context
-
 object DeviceMemoryHelper {
-
-    // 0: <=2GB, 1: 3-4GB, 2: >=6GB
     fun detectRamTier(context: Context): Int {
         val am = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         val memInfo = ActivityManager.MemoryInfo()
         am.getMemoryInfo(memInfo)
-        val totalGB = memInfo.totalMem / (1024 * 1024 * 1024) // गीगाबाइट में
+        val totalGB = memInfo.totalMem / (1024 * 1024 * 1024)
         return when {
             totalGB <= 2 -> 0
             totalGB <= 4 -> 1
