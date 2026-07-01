e63c915b6b91c81f37a14ea365230cea98609501
takrdh32-byte <takrdh32@gmail.com>
2026-07-01 08:07:43 +0530

    Update permission_helper.dart

--- lib/permission_helper.dart
+++ lib/permission_helper.dart
@@ -5,16 +5,18 @@ class PermissionHelper {
   static Future<bool> requestStoragePermissions() async {
     if (!Platform.isAndroid) return false;
 
-    final mediaImages = await Permission.photos.status;
-    if (mediaImages.isDenied) await Permission.photos.request();
-    final mediaVideos = await Permission.videos.status;
-    if (mediaVideos.isDenied) await Permission.videos.request();
+    // Android 13+ के लिए नए मीडिया परमिशन
+    if (await Permission.photos.isDenied) {
+      await Permission.photos.request();
+    }
+    if (await Permission.videos.isDenied) {
+      await Permission.videos.request();
+    }
 
-    final storage = await Permission.storage.status;
-    if (storage.isDenied) await Permission.storage.request();
-
-    final manageStorage = await Permission.manageExternalStorage.status;
-    if (manageStorage.isDenied) await Permission.manageExternalStorage.request();
+    // पुराने Android के लिए स्टोरेज परमिशन
+    if (await Permission.storage.isDenied) {
+      await Permission.storage.request();
+    }
 
     return await hasMinimumPermissions();
   }
@@ -22,8 +24,7 @@ class PermissionHelper {
   static Future<bool> hasMinimumPermissions() async {
     final photos = await Permission.photos.status;
     final storage = await Permission.storage.status;
-    final manageStorage = await Permission.manageExternalStorage.status;
-    return photos.isGranted || storage.isGranted || manageStorage.isGranted;
+    return photos.isGranted || storage.isGranted;
   }
 
   static Future<bool> isPermanentlyDenied() async {
